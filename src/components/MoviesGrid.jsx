64e0f61fddd4d090c9232b1ff3887b881e72291f
Arkapravo Chakrabarti <arkapravochakrabarti188@gmail.com>
2025-10-29 20:47:00 +0530

    State

---
+++ src/components/MoviesGrid.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "../styles.css";
+
+export default function MoviesGrid() {
+  const [movies, setMovies] = useState([])
+  return (
+  <div>
+
+  </div>
+  );
+}
+
